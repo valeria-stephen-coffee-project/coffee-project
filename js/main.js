eafde54320af7d488895004dffc1614e4602b297
Stephen Pflug <stephen@pflug.us>
2022-09-19 15:30:48 -0500

    refactored code, all funtionality complete

--- js/main.js
+++ js/main.js
@@ -3,14 +3,26 @@
 // Creating table row with coffee stats
 function renderCoffee(coffee) {
     let html = '' +
-        '<div class="col-md-4 col-12">' +
-        '<div class="card m-2">' +
+        '<div class="col-sm-6 col-lg-4 col-12">' +
+        '<div class="card text-variant bg-transparent border-0 mx-5 my-4">' +
         '<div class="card-body">' +
-        '<div class="card-title text-center">' + coffee.name + '</div>' +
-        '<div class="card-subtitle text-muted text-center">' + coffee.roast + '</div>' +
+        '<h2 class="card-title text-center brown-color">' + coffee.name + '</h2>' +
+        '<div class="card-subtitle text-center brown-color">' + coffee.roast + '</div>' +
         '</div>' +
         '</div>' +
         '</div>';
+
+    // let html = '' +
+    //     '<div class="col-sm-6 col-lg-4 col-12">' +
+    //     '<b-card ' +
+    //     'overlay ' +
+    //     'img-src="../img/milk-coffee-pic.jpg" ' +
+    //     'text-variant="white" ' +
+    //     'title="' + coffee.name + '"' +
+    //     '>' +
+    //     '<b-card-text>' + coffee.roast + '</b-card-text>' +
+    //     '</b-card>' +
+    //     '</div>';
 
     return html;
 }
@@ -19,11 +31,7 @@ function renderCoffee(coffee) {
 function renderCoffees(coffees) {
     let html = '';
     for(let i = 0; i < coffees.length; i++) {
-        // coffees.forEach(coffee => {
-        //     if(coffee.id == i+1) {
-                html += renderCoffee(coffees[i]);
-        //     }
-        // });
+        html += renderCoffee(coffees[i]);
     }
     return html;
 }
@@ -51,28 +59,35 @@ function addCoffee(e) {
 
     tbody.innerHTML = renderCoffees(coffees);
 
-    // localStorage.coffees = JSON.stringify(coffees);
+    localStorage.coffees = JSON.stringify(coffees);
 }
 
 // Coffee names and types to populate coffee list
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-let coffees =
-    [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+
+// initialzing coffees array. if local storage is empty, sets default coffees array
+let coffees = [];
+try {
+    coffees = JSON.parse(localStorage.coffees);
+} catch (error) {
+    coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
+}
+
 
 let tbody = document.querySelector('#coffeeDisplay');
 // let submitButton = document.querySelector('#submitBtn');
