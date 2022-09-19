1427a123191c312dec02e3321898831fb17e0b76
Valeria Canizales <vcanizales93@gmail.com>
2022-09-19 14:19:39 -0500

    Styling coffee project

--- js/main.js
+++ js/main.js
@@ -51,28 +51,28 @@ function addCoffee(e) {
 
     tbody.innerHTML = renderCoffees(coffees);
 
-    localStorage.coffees = JSON.stringify(coffees);
+    // localStorage.coffees = JSON.stringify(coffees);
 }
 
 // Coffee names and types to populate coffee list
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-let coffees = JSON.parse(localStorage.coffees);
-//     [
-//     {id: 1, name: 'Light City', roast: 'light'},
-//     {id: 2, name: 'Half City', roast: 'light'},
-//     {id: 3, name: 'Cinnamon', roast: 'light'},
-//     {id: 4, name: 'City', roast: 'medium'},
-//     {id: 5, name: 'American', roast: 'medium'},
-//     {id: 6, name: 'Breakfast', roast: 'medium'},
-//     {id: 7, name: 'High', roast: 'dark'},
-//     {id: 8, name: 'Continental', roast: 'dark'},
-//     {id: 9, name: 'New Orleans', roast: 'dark'},
-//     {id: 10, name: 'European', roast: 'dark'},
-//     {id: 11, name: 'Espresso', roast: 'dark'},
-//     {id: 12, name: 'Viennese', roast: 'dark'},
-//     {id: 13, name: 'Italian', roast: 'dark'},
-//     {id: 14, name: 'French', roast: 'dark'},
-// ];
+let coffees =
+    [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
 
 let tbody = document.querySelector('#coffeeDisplay');
 // let submitButton = document.querySelector('#submitBtn');
